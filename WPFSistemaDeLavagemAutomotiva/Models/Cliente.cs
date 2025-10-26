9297386cb08532da05dbf9c45af49f26f7a52d0d
João Alexandre <joao.ale25@gmail.com>
2025-10-25 22:41:31 -0300

    feat/master: Criei models Funcionario e Cliente, serão utilizados como modelos para o banco de dados

---
+++ WPFSistemaDeLavagemAutomotiva/Models/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSistemaDeLavagemAutomotiva.Models
+{
+    public class Cliente
+    {
+        private int _idCliente;
+        public int IdCliente
+        {
+            get { return _idCliente; }
+            set { _idCliente = value; }
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private string _telefone;
+        public string Telefone
+        {
+            get { return _telefone; }
+            set { _telefone = value; }
+        }
+
+        public Cliente()
+        {
+
+        }
+        
+        public Cliente(int idCliente, string nome, string email, string telefone)
+        {
+            this._idCliente = idCliente;
+            this._nome = nome;
+            this._email = email;
+            this._telefone = telefone;
+        }
+    }
+}
---
+++ WPFSistemaDeLavagemAutomotiva/Models/Funcionario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSistemaDeLavagemAutomotiva.Models
+{
+    public class Funcionario
+    {
+        private int _idFuncionario;
+        public int IdFuncionario
+        {
+            get { return _idFuncionario; }
+            set { _idFuncionario = value; }
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        private string _cargo;
+        public string Cargo
+        {
+            get { return _cargo; }
+            set { _cargo = value; }
+        }
+
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(int idFuncionario, string nome, string cargo)
+        {
+            this._idFuncionario = idFuncionario;
+            this._nome = nome;
+            this._cargo = cargo;
+        }
+    }
+}
