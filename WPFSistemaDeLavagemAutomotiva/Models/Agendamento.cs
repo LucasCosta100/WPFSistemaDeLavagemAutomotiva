0603098007a7bcff11acda5976429e64fe7007bd
LucasCosta100 <lucasdias46@outlook.com.br>
2025-10-25 22:16:14 -0300

    Feat/master: Criei as pastas estruturais do projeto

---
+++ WPFSistemaDeLavagemAutomotiva/Models/Agendamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSistemaDeLavagemAutomotiva.Models
+{
+    public class Agendamento
+    {
+        private int _idAgendamento;
+        public int IdAgendamento { get; set; }
+
+    }
+}
