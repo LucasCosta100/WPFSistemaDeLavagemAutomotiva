70c5ccb993e9528fe3986493309ba5dfcf307629
LucasCosta100 <lucasdias46@outlook.com.br>
2025-10-25 22:58:22 -0300

    feat/master: Criei a classe Servico

---
+++ WPFSistemaDeLavagemAutomotiva/Models/Servico.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSistemaDeLavagemAutomotiva.Models
+{
+    public class Servico
+    {
+        private int _idServico;
+        public int IdServico
+        {
+            get { return _idServico; }
+            set { _idServico = value; }
+        }
+        private string _nomeServico;
+        public string NomeServico
+        {
+            get { return _nomeServico; }
+            set { _nomeServico = value; }
+        }
+        private decimal _precoServico;
+        public decimal PrecoServico
+        {
+            get { return _precoServico; }
+            set { _precoServico = value; }
+        }
+
+        public Servico(){ }
+
+        public Servico(int idServico, string nomeServico, decimal precoServico)
+        {
+            this._idServico = idServico;
+            this._nomeServico = nomeServico;
+            this._precoServico = precoServico;
+        }
+    }
+}
