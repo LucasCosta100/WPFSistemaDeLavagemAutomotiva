6fa948cd1f30b52da2d7380c309475df648a995b
João Alexandre <joao.ale25@gmail.com>
2025-11-02 20:15:40 -0300

    feat/master - Implementado Controller para AgendamentoView e conteúdo para as outras duas Borders da View Agendamento

--- WPFSistemaDeLavagemAutomotiva/View/AgendamentosView.xaml.cs
+++ WPFSistemaDeLavagemAutomotiva/View/AgendamentosView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFSistemaDeLavagemAutomotiva.Controller;
 using WPFSistemaDeLavagemAutomotiva.Service;
 using WPFSistemaDeLavagemAutomotiva.View.Components;
 
@@ -22,17 +23,68 @@ namespace WPFSistemaDeLavagemAutomotiva.View
     /// </summary>
     public partial class AgendamentosView : Page
     {
-        private AgendamentoService _agendamentoService = new AgendamentoService();
+        private AgendamentosController _agendamentoController = new AgendamentosController();
         public AgendamentosView()
         {
             InitializeComponent();
-            var proximoCliente = _agendamentoService.BuscarProximoAgendamento();
-            txtNome.Text = proximoCliente.ClienteAgendado.Nome;
-            txtData.Text = (proximoCliente.DataAgendada + proximoCliente.HoraAgendamento).ToString("dd/MM/yyyy HH:mm");
-            txtServico.Text = $"Serviço: {proximoCliente.ServicoAgendado.NomeServico}";
-            txtValor.Text = $"R$ {proximoCliente.ValorTotal.ToString("F2")}";
+            BuscarProximoAgendamento();
+            ListarAgendamentosHoje();
+            BuscarTodosAgendamentos();
         }
 
+
+        public void BuscarProximoAgendamento()
+        {
+            try
+            {
+                var proximoCliente = _agendamentoController.BuscarProximoAgendamento();
+                txtNome.Text = proximoCliente.ClienteAgendado.Nome;
+                txtData.Text = (proximoCliente.DataAgendada + proximoCliente.HoraAgendamento).ToString("dd/MM/yyyy HH:mm");
+                txtServico.Text = $"Serviço: {proximoCliente.ServicoAgendado.NomeServico}";
+                txtValor.Text = $"R$ {proximoCliente.ValorTotal.ToString("F2")}";
+                txtTextoValor.Text = "Valor Total: ";
+            }
+            catch (Exception ex)
+            { //Caso não encontrar o texto, exibe uma mensagem personalizada
+                txtNome.Text = "Nenhum agendamento encontrado";
+                txtNome.FontSize = 27;
+                txtNome.TextWrapping = TextWrapping.Wrap;
+                txtNome.HorizontalAlignment = HorizontalAlignment.Center;
+                txtNome.TextAlignment = TextAlignment.Center;
+                txtNome.Margin = new Thickness(0, 50, 0, 0);
+                txtNome.Foreground = new SolidColorBrush(Color.FromRgb(255, 99, 71));
+                txtData.Text = "";
+                txtServico = null;
+                txtValor = null;
+                spBotoes.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        public void ListarAgendamentosHoje()
+        {
+            try
+            {
+                var (lista, mensagem) = _agendamentoController.ListarAgendamentosHoje();
+                txtAgendamentosHoje.Text = lista.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                txtAgendamentosHoje.Text = "0";
+            }
+        }
+
+        public void BuscarTodosAgendamentos()
+        {
+            try
+            {
+                var (lista, mensagem) = _agendamentoController.ListarAgendamentos();
+                txtAgendamentos.Text = lista.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                txtAgendamentos.Text = "0";
+            }
+        }
 
         private void lbTabelas_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
